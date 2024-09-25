28bd50923f4254eac6484ea2bdf56e29563f0dbb
Anipal <anishapal@Anishas-MacBook-Pro.local>
2024-09-24 19:46:05 -0700

    carousel new

--- static/js/imagecarousell.js
+++ static/js/imagecarousell.js
@@ -1,14 +1,11 @@
-let slideIndex = 0;
-showSlides();
+$(function() {
+  'use strict';
+  
+	$('.carousel .carousel-item[data-src]').each(function() {
+		var $this = $(this);
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+		$this.prepend([
+			'<div style="background-image: url(', $this.attr('data-src'), ')"></div>'
+		].join(''));
+	});
+});
