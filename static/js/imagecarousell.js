f71a6a314541f60bac8d02a422e7351f99429ce8
Anipal <anishapal@Anishas-MacBook-Pro.local>
2024-09-24 20:05:39 -0700

    carousel different

--- static/js/imagecarousell.js
+++ static/js/imagecarousell.js
@@ -1,11 +1,28 @@
-$(function() {
-  'use strict';
-  
-	$('.carousel .carousel-item[data-src]').each(function() {
-		var $this = $(this);
+let slideIndex = 1;
+showSlides(slideIndex);
 
-		$this.prepend([
-			'<div style="background-image: url(', $this.attr('data-src'), ')"></div>'
-		].join(''));
-	});
-});
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
