963ddb96642bc2a1f6753f6289a169cacd1fe8a3
Anipal <anishapal@Anishas-MacBook-Pro.local>
2024-09-24 20:41:35 -0700

    carousel different w3

--- static/js/imagecarousell.js
+++ static/js/imagecarousell.js
@@ -1,26 +1,14 @@
 let slideIndex = 0;
-let slides = document.getElementsByClassName("mySlides");
+showSlides();
 
 function showSlides() {
   let i;
-
-  // Loop through all the slides and reset their display state
+  let slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
-
-  // Increment slide index, reset to 0 if it exceeds the number of slides
   slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1; // Reset to first slide
-  }
-
-  // Show the current slide
-  slides[slideIndex - 1].style.display = "block";  
-
-  // Repeat the process after 3 seconds
-  setTimeout(showSlides, 3000); // Change image every 3 seconds
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-
-// Start the carousel
-showSlides();
