59058e569fc968eacb2e55c32e6bd44e813cd304
Anipal <anishapal@Anishas-MacBook-Pro.local>
2024-09-24 23:47:31 -0700

    show slides

--- static/js/imagecarousell.js
+++ static/js/imagecarousell.js
@@ -4,11 +4,19 @@ showSlides();
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  
+  // Hide all slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
+  // Increment slideIndex and wrap around
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
+  if (slideIndex > slides.length) {
+    slideIndex = 1; // Reset to 1 if exceeds
+  }
+
+  // Show the current slide
+  slides[slideIndex - 1].style.display = "block"; // Display the correct slide
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
