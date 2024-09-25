63a461ab96c75c44cb53b66415a0bb5f1a7775f8
Anipal <anishapal@Anishas-MacBook-Pro.local>
2024-09-25 00:19:38 -0700

    carousel fix

--- static/js/imagecarousell.js
+++ static/js/imagecarousell.js
@@ -1,22 +1,28 @@
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlides() {
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  
-  // Hide all slides
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-
-  // Increment slideIndex and wrap around
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1; // Reset to 1 if exceeds
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-
-  // Show the current slide
-  slides[slideIndex - 1].style.display = "block"; // Display the correct slide
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
