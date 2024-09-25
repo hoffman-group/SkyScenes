708070da88043fad8d097bac39e544d1bc582d32
Anipal <anishapal@Anishas-MacBook-Pro.local>
2024-09-24 20:11:25 -0700

    carousel different 1

--- static/js/imagecarousell.js
+++ static/js/imagecarousell.js
@@ -15,14 +15,27 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  
+  if (n > slides.length) {
+    slideIndex = 1;
   }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  
+  slides[slideIndex - 1].style.display = "block";  
+  dots[slideIndex - 1].className += " active";
 }
+
+// Auto-play the carousel every 5 seconds
+setInterval(() => {
+  plusSlides(1);
+}, 5000);
